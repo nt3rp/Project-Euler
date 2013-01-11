b62d8b36730544816194cc668a5a17e45986f878
Nicholas Terwoord <nicholas.terwoord+code@gmail.com>
2013-01-11 00:13:39 -0500

    Updated question 10.

--- completed/q10.py
+++ completed/q10.py
@@ -14,3 +14,22 @@ while( (p*p) < value):
     p = primes[i+1]
     
 print primes[-1], sum(primes), clock() - start;
+
+def main():
+    """Find the sum of all the primes below two million."""
+    # Using something like the sieve of eratosthenes
+    # http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+
+    value = 2000000
+    p = 2
+    primes = range(p, value)
+
+    while ((p*p) < value):
+        i = primes.index(p)
+        primes[i+1:] = filter(lambda x: x % p != 0, primes[i:])
+        p = primes[i+1]
+
+    return sum(primes)
+
+if __name__ == '__main__':
+    print(main())
